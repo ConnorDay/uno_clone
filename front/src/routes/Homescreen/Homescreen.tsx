085fbaac68e9866c85d19d88604a4edd928d3937
Connor Day <connor.t.day@gmail.com>
2022-06-19 18:43:06 -0400

    nothing really changed. Just need to include a .env with REACT_APP_HOST and REACT_APP_PORT

--- front/src/routes/Homescreen/Homescreen.tsx
+++ front/src/routes/Homescreen/Homescreen.tsx
@@ -4,6 +4,7 @@ import "./Homescreen.css";
 function Homescreen() {
     return (
         <div className="homescreen">
+            <p>{process.env.REACT_APP_HOST}</p>
             <CodeEnter />
         </div>
     );
