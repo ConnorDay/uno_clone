160889585410c6503f4184014dbcfeda023f6652
Connor Day <connor.t.day@gmail.com>
2022-07-05 12:58:36 -0400

    Hands are now sorted on the client end

--- front/src/components/Hand/Hand.tsx
+++ front/src/components/Hand/Hand.tsx
@@ -6,6 +6,18 @@ type Props = {
 function Hand(props: Props) {
     const { cards } = props;
 
+    cards.sort((a, b) => {
+        if (a.color === b.color) {
+            if (a.value === b.value) {
+                return 0;
+            }
+            return a.value < b.value ? -1 : 1;
+        }
+        if (a.color === "special") return 1;
+        if (b.color === "special") return -1;
+        return a.color < b.color ? -1 : 1;
+    });
+
     return (
         <div>
             {cards.map((card) => {
