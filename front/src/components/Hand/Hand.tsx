075dc536d06e8662a2e16eea1653919d085af981
Connor Day <connor.t.day@gmail.com>
2022-07-05 12:59:53 -0400

    add some comments

--- front/src/components/Hand/Hand.tsx
+++ front/src/components/Hand/Hand.tsx
@@ -6,15 +6,20 @@ type Props = {
 function Hand(props: Props) {
     const { cards } = props;
 
+    //Sort the hand by color first, then value
     cards.sort((a, b) => {
+        //Sort by value
         if (a.color === b.color) {
-            if (a.value === b.value) {
-                return 0;
-            }
+            if (a.value === b.value) return 0;
+
             return a.value < b.value ? -1 : 1;
         }
+
+        //Make sure "special" cards appear at the end
         if (a.color === "special") return 1;
         if (b.color === "special") return -1;
+
+        //Sort by color
         return a.color < b.color ? -1 : 1;
     });
 
